af5c704db7b914a8c07654e334da3501669af229
Waldemar148 <vmykolaichyk@gmail.com>
2019-03-05 17:14:06 +0200

    Changed files to es6 style

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -1,81 +1,83 @@
 'use strict';
 
-var path = require('path');
-var webpack = require('webpack');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
-var StatsPlugin = require('stats-webpack-plugin');
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const StatsPlugin = require('stats-webpack-plugin');
 
 module.exports = {
-    entry: [
-        path.join(__dirname, 'src/index.js')
+  entry: [path.join(__dirname, 'src/index.js')],
+  output: {
+    path: path.join(__dirname, '/public/'),
+    filename: '[name]-[hash].min.js',
+    publicPath: '/'
+  },
+  plugins: [
+    new webpack.optimize.OccurenceOrderPlugin(),
+
+    new HtmlWebpackPlugin({
+      template: 'src/index.tpl.html',
+      inject: 'body',
+      filename: 'index.html'
+    }),
+
+    new ExtractTextPlugin('[name]-[hash].min.css'),
+
+    new webpack.optimize.UglifyJsPlugin({
+      compressor: {
+        warnings: false,
+        screw_ie8: true
+      }
+    }),
+
+    new StatsPlugin('webpack.stats.json', {
+      source: false,
+      modules: false
+    }),
+
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('production')
+    })
+  ],
+
+  eslint: {
+    configFile: '.eslintrc',
+    failOnWarning: false,
+    failOnError: true
+  },
+
+  module: {
+    preLoaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'eslint'
+      }
     ],
-    output: {
-        path: path.join(__dirname, '/public/'),
-        filename: '[name]-[hash].min.js',
-        publicPath: '/'
-    },
-    plugins: [
-        new webpack.optimize.OccurenceOrderPlugin(),
-
-        new HtmlWebpackPlugin({
-            template: 'src/index.tpl.html',
-            inject: 'body',
-            filename: 'index.html'
-        }),
-
-        new ExtractTextPlugin('[name]-[hash].min.css'),
-
-        new webpack.optimize.UglifyJsPlugin({
-            compressor: {
-                warnings: false,
-                screw_ie8: true
-            }
-        }),
-
-        new StatsPlugin('webpack.stats.json', {
-            source: false,
-            modules: false
-        }),
-
-        new webpack.DefinePlugin({
-            'process.env.NODE_ENV': JSON.stringify('production')
-        })
-    ],
-
-    eslint: {
-        configFile: '.eslintrc',
-        failOnWarning: false,
-        failOnError: true
-    },
-
-    module: {
-        preLoaders: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loader: 'eslint'
-            }
-        ],
-        loaders: [{
-            test: /\.js?$/,
-            exclude: /node_modules/,
-            loader: 'babel'
-        }, {
-            test: /\.json?$/,
-            loader: 'json'
-        }, {
-            test: /\.scss$/,
-            loader: ExtractTextPlugin.extract('style', 'css!postcss!sass')
-        },{
-            test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-            loader: "url-loader?limit=10000&minetype=application/font-woff"
-        },{
-            test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-            loader: "file-loader"
-        }]
-    },
-    postcss: [
-        require('autoprefixer')
+    loaders: [
+      {
+        test: /\.js?$/,
+        exclude: /node_modules/,
+        loader: 'babel'
+      },
+      {
+        test: /\.json?$/,
+        loader: 'json'
+      },
+      {
+        test: /\.scss$/,
+        loader: ExtractTextPlugin.extract('style', 'css!postcss!sass')
+      },
+      {
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: 'url-loader?limit=10000&minetype=application/font-woff'
+      },
+      {
+        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: 'file-loader'
+      }
     ]
+  },
+  postcss: [require('autoprefixer')]
 };
